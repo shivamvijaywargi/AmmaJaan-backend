5667333d97d25bef3de3748e4f98ea491b31911d
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-01-23 09:46:13 +0530

    BE: Order schema/model done😁

--- server/src/models/Order.model.ts
+++ server/src/models/Order.model.ts
@@ -1,6 +1,51 @@
 import { Schema, model } from "mongoose";
 
-const orderSchema: Schema = new Schema({});
+const orderSchema: Schema = new Schema(
+  {
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: Number,
+        price: Number,
+      },
+    ],
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+    },
+    paymentMethod: {
+      type: String,
+      required: [true, "Payment method is required"],
+    },
+    total: {
+      type: Number,
+      required: [true, "Total price is required"],
+    },
+    coupon: String,
+    transactionId: String,
+    status: {
+      type: String,
+      enum: ["ORDERED", "SHIPPED", "DELIVERED", "CANCELLED"],
+      default: "ORDERED",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Order = model("Order", orderSchema);
 
