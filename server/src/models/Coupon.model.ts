c0b1b6255c68308f17f3af04761ef5ce10ceb745
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-01-23 09:30:38 +0530

    BE: Coupon schema/model done✅

--- server/src/models/Coupon.model.ts
+++ server/src/models/Coupon.model.ts
@@ -1,6 +1,29 @@
 import { Schema, model } from "mongoose";
 
-const couponSchema: Schema = new Schema({});
+const couponSchema: Schema = new Schema(
+  {
+    couponCode: {
+      type: String,
+      minlength: [5, "Coupon code must be atleast 5 characters long"],
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    expires: {
+      type: Date,
+      default: +new Date() + 7 * 24 * 60 * 60 * 1000, // Expire in 7 days from creation
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Coupon = model("Coupon", couponSchema);
 
