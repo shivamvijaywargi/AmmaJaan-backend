aa4c321ada5e471a845bfcb7cd5b0d82ee5516f5
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-01-23 09:36:46 +0530

    BE: Address schema/model done👌

--- server/src/models/Address.model.ts
+++ server/src/models/Address.model.ts
@@ -1,6 +1,47 @@
 import { Schema, model } from "mongoose";
 
-const addressSchema: Schema = new Schema({});
+const addressSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minlength: [5, "Name must be atleast 5 characters long"],
+      maxlength: [25, "Name cannot be more than 25 characters"],
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: true,
+      minlength: [10, "Phone number cannot be less than 10 digits"],
+      maxlength: [15, "Phone number cannot be more than 15 digits"],
+    },
+    houseNumber: {
+      type: String,
+      required: [true, "House number is required"],
+    },
+    city: {
+      type: String,
+      required: [true, "City is required"],
+      trim: true,
+    },
+    state: {
+      type: String,
+      required: [true, "State is required"],
+      trim: true,
+    },
+    pinCode: {
+      type: String,
+      trim: true,
+    },
+    isPrimary: {
+      type: Boolean,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Address = model("Address", addressSchema);
 
