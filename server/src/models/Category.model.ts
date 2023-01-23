79c0303f4baa59ee48210b9b7dcd5f74d9e6602e
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-01-23 09:32:54 +0530

    BE: Category schema/model done😁

--- server/src/models/Category.model.ts
+++ server/src/models/Category.model.ts
@@ -1,6 +1,29 @@
 import { Schema, model } from "mongoose";
 
-const categorySchema: Schema = new Schema({});
+const categorySchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      minlength: [3, "Category name must be atleast 3 characters long"],
+      maxlength: [20, "Category name cannot be more than 20 characters"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      minlength: [15, "Description must be atleast 15 characters long"],
+    },
+    slug: {
+      type: String,
+      required: [true, "Slug is required"],
+      unique: true,
+      lowercase: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Category = model("Category", categorySchema);
 
