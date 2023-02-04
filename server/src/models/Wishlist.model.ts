8f994676793503352535ec281f9e71591954ef06
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-02-04 08:10:08 +0530

    BE: Created Wishlist model/schema✅

---
+++ server/src/models/Wishlist.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const wishlistSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Wishlist = model("Wishlist", wishlistSchema);
+
+export default Wishlist;
