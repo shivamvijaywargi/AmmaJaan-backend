8518ee9a85c9df23ee5efae4ebf29bc11e814a3e
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-02-13 17:55:55 +0530

    BE: Updated UpdatedUserSchema with partial()-untested

--- server/src/schemas/product.schema.ts
+++ server/src/schemas/product.schema.ts
@@ -2,64 +2,21 @@ import { Types } from 'mongoose';
 import { z } from 'zod';
 
 // Cannot figure out how to use this with formidable, I will check this later if possible
-// export const CreateProductSchema = z.object({
-//   body: z.object({
-//     title: z
-//       .string({
-//         required_error: 'Title is required',
-//         invalid_type_error: 'Title must be a string',
-//       })
-//       .min(5)
-//       .max(55),
-//     description: z
-//       .string({
-//         required_error: 'Description is required',
-//         invalid_type_error: 'Description must be a string',
-//       })
-//       .min(10),
-//     shortDescription: z
-//       .string()
-//       .min(20, {
-//         message: 'Short description must be atleast 20 characters long',
-//       })
-//       .optional(),
-//     price: z
-//       .number({
-//         required_error: 'Price is required',
-//         invalid_type_error: 'Price must be a positive number',
-//       })
-//       .min(1, {
-//         message: 'Price must be atleast 1',
-//       })
-//       .positive(),
-//     quantity: z
-//       .number({
-//         invalid_type_error: 'Quantity must be a positive number',
-//       })
-//       .positive()
-//       .max(99999, {
-//         message: 'Quantity must be less than or equal to 99999',
-//       })
-//       .optional(),
-//     label: z.enum(['Hot', 'New', 'Best Selling']).optional(),
-//     inStock: z.boolean().optional(),
-//     category: z.instanceOf(Types.ObjectId),
-//   }),
-// });
-
-export const productQuerySchema = z.object({
-  query: z.object({
-    search: z.string().optional(),
-    sort: z.string().optional(),
-    page: z.string().optional(),
-    limit: z.string().optional(),
-  }),
-});
-
-export const UpdateProductSchema = z.object({
+export const CreateProductSchema = z.object({
   body: z.object({
-    title: z.string().min(5).max(55).optional(),
-    description: z.string().min(10).optional(),
+    title: z
+      .string({
+        required_error: 'Title is required',
+        invalid_type_error: 'Title must be a string',
+      })
+      .min(5)
+      .max(55),
+    description: z
+      .string({
+        required_error: 'Description is required',
+        invalid_type_error: 'Description must be a string',
+      })
+      .min(10),
     shortDescription: z
       .string()
       .min(20, {
@@ -71,11 +28,10 @@ export const UpdateProductSchema = z.object({
         required_error: 'Price is required',
         invalid_type_error: 'Price must be a positive number',
       })
-      .positive()
       .min(1, {
         message: 'Price must be atleast 1',
       })
-      .optional(),
+      .positive(),
     quantity: z
       .number({
         invalid_type_error: 'Quantity must be a positive number',
@@ -87,13 +43,17 @@ export const UpdateProductSchema = z.object({
       .optional(),
     label: z.enum(['Hot', 'New', 'Best Selling']).optional(),
     inStock: z.boolean().optional(),
-    category: z
-      .instanceof(Types.ObjectId) // Use when the type is mongoose.Schema.Types.ObjectId
-      // .transform((id) => id.toString())
-      .optional(),
-    // category: z
-    //   .instanceof(Types.ObjectId)
-    //   .transform((id) => id.toString()) // Do this if the check is in NextJS
-    //   .optional(),
+    category: z.instanceof(Types.ObjectId),
   }),
 });
+
+export const productQuerySchema = z.object({
+  query: z.object({
+    search: z.string().optional(),
+    sort: z.string().optional(),
+    page: z.string().optional(),
+    limit: z.string().optional(),
+  }),
+});
+
+export const UpdateProductSchema = CreateProductSchema.partial();
