181324a7b0276fba7de2f5214f9232d02c3d5abd
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-03-04 19:18:20 +0530

    DEPS: Added Stripe dependency and env vars➕

--- src/env.d.ts
+++ src/env.d.ts
@@ -22,6 +22,9 @@ const envVariables = z.object({
   CLOUDINARY_CLOUD_NAME: z.string(),
   CLOUDINARY_API_KEY: z.string(),
   CLOUDINARY_API_SECRET: z.string(),
+
+  STRIPE_PUBLISHABLE_KEY: z.string(),
+  STRIPE_SECRET_KEY: z.string(),
 });
 
 envVariables.parse(process.env);
