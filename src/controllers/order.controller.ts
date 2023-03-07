559ff0b0d3e94ed5ea19cd3ecc2819faa251787d
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-03-07 12:56:00 +0530

    ADD: Added pagination to logged in user orders✨

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -51,10 +51,18 @@ export const getAllOrdersAdmin: RequestHandler = asyncHandler(
  */
 export const getAllLoggedInUserOrders: RequestHandler = asyncHandler(
   async (req, res, next) => {
+    // Pagination setup
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 50;
+
+    const skip = (page - 1) * limit;
+
     const orders = await Order.find({ user: req.user?.user_id })
       .populate('user address coupon')
       .populate('address') // FIXME: For some reason address is not getting populated, need to check
-      .populate({ path: 'products.product', select: 'title' });
+      .populate({ path: 'products.product', select: 'title' })
+      .skip(skip)
+      .limit(limit);
 
     if (!orders.length) {
       return next(
@@ -62,10 +70,17 @@ export const getAllLoggedInUserOrders: RequestHandler = asyncHandler(
       );
     }
 
+    const count = await Order.find({
+      user: req.user?.user_id,
+    }).countDocuments();
+
     res.status(200).json({
       success: true,
       message: 'All user orders fetched successfully',
       orders,
+      totalPages: Math.ceil(count / limit),
+      currentPage: Number(page),
+      count,
     });
   },
 );
