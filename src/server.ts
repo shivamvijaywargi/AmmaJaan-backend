8b273f1a9099b8a5af786f513ef7da482f1a4bd7
Shivam Vijaywargi <vjshivam5@gmail.com>
2023-03-20 17:13:04 +0530

    Added link to clustering with app.js & server.js

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from '@/app';
-import connectToDB from '@/configs/dbConn';
-import Logger from '@/utils/logger';
+import app from "@/app";
+import connectToDB from "@/configs/dbConn";
+import Logger from "@/utils/logger";
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,7 +10,9 @@ app.listen(PORT, async () => {
 });
 
 // log unhandled rejections
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   Logger.error(err);
   process.exit(1);
 });
+
+// Try this once for cluster using app.js and server.js file: https://www.softwareontheroad.com/nodejs-scalability-issues/
