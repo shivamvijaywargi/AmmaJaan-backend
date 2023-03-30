293704e61bcd560890e3073fe351baa2eea9336b
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-03-30 17:14:54 +0530

    BUILD: Update docker-compose& add local MongoURI🔧

--- src/configs/dbConn.ts
+++ src/configs/dbConn.ts
@@ -6,7 +6,10 @@ mongoose.set('runValidators', true); // To run mongoose Schema validations upon 
 
 const connectToDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    const MONGO_URI =
+      process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/ammajaan';
+
+    const conn = await mongoose.connect(MONGO_URI);
 
     if (conn) {
       Logger.info(`Connected to DB: ${conn.connection.host}`);
