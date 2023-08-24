8e96413c73920cd31e958c9b1daf70d340de19d9
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-08-24 18:51:24 +0530

    fix: sameSite: none from None

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -11,7 +11,7 @@ import sendEmail from '@/utils/sendEmail';
 const cookieOptions = {
   httpOnly: true,
   maxAge: 7 * 24 * 60 * 60 * 1000,
-  sameSite: 'None',
+  sameSite: 'none',
   secure: process.env.NODE_ENV === 'production' ? true : false,
 };
 
